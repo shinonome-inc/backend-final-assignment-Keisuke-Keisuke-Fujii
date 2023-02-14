ecbe6a46c964fb0a105eb1a69a99cc0ab37cf2f8
Kei <k.fuji.22.8.605@4nonome.com>
2023-02-14 18:40:30 +0900

    修正:ログアウトの隣のユーザプロフィールへの遷移ボタンに関するhtml記述を修正

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, DetailView, TemplateView
 
 from .forms import LoginForm, SignupForm
 
