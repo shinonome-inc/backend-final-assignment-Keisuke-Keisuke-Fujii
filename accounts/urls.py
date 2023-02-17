ede6b54a492ea59ec00d1116685a2584a9f2fd13
Kei <k.fuji.22.8.605@4nonome.com>
2023-02-17 20:50:00 +0900

    修正:accountsのurlsviewsのimport修正,viewsのLogoutViewとLoginView削除,as_viewメソッド修正

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,21 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from . import views
+from . import forms, views
 
 # 逆引きなどのための名称はapp_name:nameなのでaccounts:signupとなる．
 # プロジェクトmysiteのurlsからここに来て，もう一度URLを調べpath一覧に一致するurlがあればそのviewメソッド実行
 app_name = "accounts"
 urlpatterns = [
     path("signup/", views.SignupView.as_view(), name="signup"),
-    path("login/", views.UserLoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path(
+        "login/",
+        LoginView.as_view(
+            form_class=forms.LoginForm, template_name="accounts/login.html"
+        ),
+        name="login",
+    ),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("<str:username>/", views.UserProfileView.as_view(), name="user_profile"),
     # path('<str:username>/follow/', views.FollowView.as_view(), name='follow'),
     # path('<str:username>/unfollow/', views.UnFollowView, name='unfollow'),
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DetailView, TemplateView
@@ -28,11 +28,6 @@ class SignupView(CreateView):
         user = authenticate(self.request, username=username, password=password)
         login(self.request, user)
         return response
-
-
-class UserLoginView(LoginView):
-    form_class = LoginForm
-    template_name = "accounts/login.html"
 
 
 class UserProfileView(LoginRequiredMixin, TemplateView):
