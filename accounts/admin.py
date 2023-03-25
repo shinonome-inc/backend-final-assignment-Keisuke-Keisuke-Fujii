04bd3a9e79eb4685a43e2b2cd7391614e2b52d1a
Kei <k.fuji.22.8.605@4nonome.com>
2023-03-25 22:02:50 +0900

    追加:.isort.cfg

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
 
 # Register your models here.
-from django.contrib.auth import get_user_model
 
 
 CustomUser = get_user_model()
