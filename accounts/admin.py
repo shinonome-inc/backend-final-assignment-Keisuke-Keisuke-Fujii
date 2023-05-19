3808314062a88e30f6eac989ed81d9c9f94c9ba1
Kei <k.fuji.22.8.605@4nonome.com>
2023-05-19 15:36:54 +0900

    追加:フォロー機能のモデル

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 
+from .models import FriendShip
+
 # Register your models here.
 
 
 CustomUser = get_user_model()
 admin.site.register(CustomUser)
+admin.site.register(FriendShip)
--- accounts/models.py
+++ accounts/models.py
@@ -7,5 +7,25 @@ class CustomUser(AbstractUser):
     email = models.EmailField()
 
 
-# class FriendShip(models.Model):
-# passa
+class FriendShip(models.Model):
+    """
+    related_nameでORMで参照できるようにする。
+    1つのモデルに対し複数のForeignKeyフィールドを設定する場合このオプションが必須
+    """
+
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="follower")
+    following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="following")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "フォロワー/フォロー"
+        constraints = [
+            # 複合ユニーク制約、filedsに指定したカラムの値が同じレコードの存在を許可しない
+            models.UniqueConstraint(
+                fields=["follower", "following"],
+                name="unique_friendship",
+            )
+        ]
+
+        def __str__(self):
+            return f"{self.follower} → {self.following}"
