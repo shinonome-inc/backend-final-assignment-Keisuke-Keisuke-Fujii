bea61530a1e2e926aedf9c240f62503062cc7893
Kei <k.fuji.22.8.605@4nonome.com>
2023-06-18 22:55:42 +0900

    追加:FriendShipモデルのrelated_nameをわかりやすく修正

--- accounts/models.py
+++ accounts/models.py
@@ -13,11 +13,24 @@ class FriendShip(models.Model):
     1つのモデルに対し複数のForeignKeyフィールドを設定する場合このオプションが必須
     フィールドがfollower=Kei, following=Kyokoというレコードがあったら、
     「KeiがKyokoのことをフォローすることでKyokoのフォロワーになっているという関係」
-    を示す。
+    を示す。followingはfolloweeの方がわかりやすいかも
+
+    なお、related_nameはターミナルのシェル(python manage.py shell)操作などで用いる。
+    フィールド名と逆なのに注意。
+    シェルの書き方
+    from accounts.models import CustomUser (FriendShipの親モデル)
+    user=CustomUser.objects.get(username="Kyoko") なおKyokoのid=2
+    user.following.all()
+    ↑このfollowingはrelated_nameでFriendShipのDBとは逆にこちらは素直に
+    user(Kyoko)「が」フォローしている(FriendShipモデルのfollowerフィールドがKyokoである)FriendShipオブジェクトを抽出
+
+    user.follower.all()
+    ↑このfollowerはrelated_nameでFriendShipのDBとは逆にこちらは素直に
+    user(Kyoko)「を」フォローしている(FriendShipモデルのfollowingフィールドがKyokoである)オブジェクト抽出
     """
 
-    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="follower")
-    following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="following")
+    follower = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="following")
+    following = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="follower")
     created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
